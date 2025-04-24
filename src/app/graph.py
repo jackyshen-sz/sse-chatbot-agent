53019dc33f0bc2a011f95579b2cf21e5cf866651
Jacky shen <jacky.shen@paradm.com>
2025-04-24 14:46:57 +0800

    Update graph.py

--- src/app/graph.py
+++ src/app/graph.py
@@ -1,34 +1,44 @@
-from langchain_openai import ChatOpenAI
+from langchain_together import ChatTogether
 from langgraph.prebuilt import create_react_agent
 from langgraph_supervisor import create_supervisor
 
+TOGETHER_API_KEY = "fb2dbf62bf0bc04bcebd0211dced579561f8b90fbfc27de4f40675c43a468635"
+model = ChatTogether(
+  model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+  temperature=0.7,
+  api_key=TOGETHER_API_KEY
+)
+
+
 def book_hotel(hotel_name: str):
-    """Book a hotel"""
-    return f"Successfully booked a stay at {hotel_name}."
+  """Book a hotel"""
+  return f"Successfully booked a stay at {hotel_name}."
+
 
 def book_flight(from_airport: str, to_airport: str):
-    """Book a flight"""
-    return f"Successfully booked a flight from {from_airport} to {to_airport}."
+  """Book a flight"""
+  return f"Successfully booked a flight from {from_airport} to {to_airport}."
+
 
 flight_assistant = create_react_agent(
-    model="openai:gpt-4o",
-    tools=[book_flight],
-    prompt="You are a flight booking assistant",
-    name="flight_assistant"
+  model=model,
+  tools=[book_flight],
+  prompt="You are a flight booking assistant",
+  name="flight_assistant"
 )
 
 hotel_assistant = create_react_agent(
-    model="openai:gpt-4o",
-    tools=[book_hotel],
-    prompt="You are a hotel booking assistant",
-    name="hotel_assistant"
+  model=model,
+  tools=[book_hotel],
+  prompt="You are a hotel booking assistant",
+  name="hotel_assistant"
 )
 
 graph = create_supervisor(
-    agents=[flight_assistant, hotel_assistant],
-    model=ChatOpenAI(model="gpt-4o"),
-    prompt=(
-        "You manage a hotel booking assistant and a"
-        "flight booking assistant. Assign work to them."
-    )
+  agents=[flight_assistant, hotel_assistant],
+  model=model,
+  prompt=(
+    "You manage a hotel booking assistant and a"
+    "flight booking assistant. Assign work to them."
+  )
 ).compile()
